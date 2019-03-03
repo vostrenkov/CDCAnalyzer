758bdc748788609f69f79a6ba87e6e238007cbae
Yury Vostrenkov <yuvostrenkov@gmail.com>
2019-03-03 13:47:43 +0300

    initial commit

---
+++ CDCAnalyzer/Analyzer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Threading;
+
+namespace CDCAnalyzer
+{
+    public class Analyzer
+    {
+
+        SerialPort ComPort { get; set; }
+        public int Baud { get; set; }
+        public int ReceivedCnt { get; set; }
+        public List<string> PortList { get; set; }
+        public string SelectedPort { get;  set; }
+        public bool ConnectionState { get; set; }
+
+        public int CurrentSpeed { get; set; }
+        public float AverageSpeed { get; set; }
+
+        private int LastReceivedCnt = 0;
+        private DateTime FirstTimestamp;
+        private DateTime LastTimestamp;
+
+        private byte[] SerialBuffer = new byte[1000000];
+        private int SerialCnt = 0;
+        private int SerialPos = 0;
+
+        private DispatcherTimer Timer;
+
+        public Analyzer()
+        {
+
+            ComPort = new SerialPort();
+            Baud = 115200;
+            ReceivedCnt = 0;
+            PortList = SerialPort.GetPortNames().ToList<string>();
+            ConnectionState = false;
+
+            CurrentSpeed = 0;
+            AverageSpeed = 0;
+
+            Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromMilliseconds(250);
+            Timer.Tick += this.TimerTickHandler;
+            Timer.Start();
+
+        }
+
+    public void ChangeConnectionState (bool state)
+        {
+            if (ComPort.IsOpen == false && state == true && SelectedPort != null)
+            {
+                try
+                {
+                    ComPort.PortName = SelectedPort;
+                    ComPort.Open();
+                    ComPort.DataReceived += DataReceivedHandler;
+                    ConnectionState = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: {0}", ex.Message);
+                }
+            }
+            else if (ComPort.IsOpen == true && state == false)
+            {
+                ComPort.Close();
+                ComPort.DataReceived -= DataReceivedHandler;
+                ConnectionState = false;
+            }
+            else if (SelectedPort != null)
+            {
+                ConnectionState = state;
+            }
+        }
+
+        void DataReceivedHandler (object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort) sender;
+            int cnt = sp.BytesToRead;
+
+            if (FirstTimestamp.Ticks == 0)
+            {
+                FirstTimestamp = DateTime.Now;
+            }
+
+            sp.Read(SerialBuffer, 0, cnt);
+            
+            ReceivedCnt += cnt;
+
+            LastTimestamp = DateTime.Now;
+
+            if (FirstTimestamp.Ticks != LastTimestamp.Ticks)
+                AverageSpeed = (ReceivedCnt) *  ((float) TimeSpan.TicksPerSecond / (LastTimestamp.Ticks - FirstTimestamp.Ticks));
+        }
+
+        void TimerTickHandler(object sender, EventArgs e)
+        {
+            DispatcherTimer dt = (DispatcherTimer)sender;
+
+            CurrentSpeed = (ReceivedCnt - LastReceivedCnt) * 4;
+
+            LastReceivedCnt = ReceivedCnt;
+        }
+
+    }
+}
---
+++ CDCAnalyzer/AnalyzerVM.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.IO.Ports;
+
+namespace CDCAnalyzer
+{
+    public class AnalyzerVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Analyzer analyzer;
+        DispatcherTimer timer;
+        private UiCommand ButtonConnectCommand;
+        private UiCommand ButtonClearCommand;
+
+        public List<string> PortListVM
+        {
+            get
+            {
+                return analyzer.PortList;
+            }
+        }
+        public string SelectedPortVM
+        {
+            set
+            {
+                if (value != null)
+                {
+                    analyzer.SelectedPort = value;
+                }
+            }
+        }
+        //public string BaudVM
+        //{
+        //    get
+        //    {
+        //        return analyzer.Baud.ToString();
+        //    }
+        //    set
+        //    {
+        //        analyzer.Baud = int.Parse(value);
+        //        PropertyChanged(this, new PropertyChangedEventArgs("BaudVM"));
+        //    }
+        //}
+
+        public string BytesReceivedVM
+        {
+            get
+            {
+                return analyzer.ReceivedCnt.ToString();
+            }
+        }
+        public string CurrentSpeedVM
+        {
+            get
+            {
+                return analyzer.CurrentSpeed.ToString();
+            }
+
+        }
+        public string AverageSpeedVM
+        {
+            get
+            {
+                return analyzer.AverageSpeed.ToString();
+            }
+
+        }
+        public string ConnectionStateVM
+        {
+            get
+            {
+                if (analyzer.ConnectionState == true)
+                {
+                    return "Disconnect";
+                }
+                else
+                {
+                    return "Connect";
+                }
+            }
+        }
+
+        public ICommand ButtonConnect_Click
+        {
+            get
+            {
+                if (ButtonConnectCommand == null)
+                {
+                    ButtonConnectCommand = new UiCommand((obj) => this.ConnectRequest(obj));
+                }
+                return ButtonConnectCommand;
+            }
+        }
+
+        public ICommand ButtonClear_Click
+        {
+            get
+            {
+                if (ButtonClearCommand == null)
+                {
+                    ButtonClearCommand = new UiCommand((obj) => this.ClearRequest(obj));
+                }
+                return ButtonClearCommand;
+            }
+        }
+
+
+        public AnalyzerVM ()
+        {
+            analyzer = new Analyzer();
+
+            //Configure and start timer
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(50);
+            timer.Tick += TimerTickHandler;
+            timer.Start();
+
+        }
+
+        // Update window with received data
+        public void TimerTickHandler(object sender, EventArgs e)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs("BytesReceivedVM"));
+            PropertyChanged(this, new PropertyChangedEventArgs("CurrentSpeedVM"));
+            PropertyChanged(this, new PropertyChangedEventArgs("AverageSpeedVM"));
+        }
+
+        public void ConnectRequest (object parameter)
+        {
+            if (analyzer.ConnectionState == true)
+            {
+                analyzer.ChangeConnectionState(false);
+            }
+            else
+            {
+                analyzer.ChangeConnectionState(true);
+            }
+            PropertyChanged(this, new PropertyChangedEventArgs("ConnectionStateVM"));
+        }
+
+        public void ClearRequest (object parameter)
+        {
+            analyzer.AverageSpeed = 0;
+            analyzer.CurrentSpeed = 0;
+            analyzer.ReceivedCnt = 0;
+        }
+    }
+}
